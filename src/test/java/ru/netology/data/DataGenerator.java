e7a8f46906ffe6d790baff687e51fc6f64232ef6
IvanVorobev <vivancho@list.ru>
2020-07-28 13:32:32 +0300

    DataGenerator and Registration added

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,30 @@
+package ru.netology.data;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+    private static RequestSpecification specification = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(7777)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static void requestForm(Registration registration) {
+        given()
+                .spec(specification)
+                .body(registration)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+}
+
